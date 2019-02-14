ba15f2dfd76a0e97292acb279161ce1735428249
Divyaansh Jain <46771398+Divyaansh313@users.noreply.github.com>
2019-02-14 11:14:17 +0530

    Update file_2.cpp

--- file_2.cpp
+++ file_2.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
     int i;
+    cout<<"Basic loop operation"<<endl;
     cout << "\n\n Find the first 10 natural numbers:\n";
     cout << " The natural numbers are: \n";
     for (i = 1; i <= 10; i++) 
